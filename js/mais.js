d30ac58a9863aa2b542a4ec3ed746369f177d34c
lsh <viperact26@gmail.com>
2022-10-03 16:47:55 +0900

    update

--- js/mais.js
+++ js/mais.js
@@ -23,3 +23,43 @@ function chgTab(i1, i2) {
   tm[i2].classList.remove("on");
   tc[i2].classList.remove("on");
 }
+
+function leftPad(num) {
+  if (num < 10) {
+    return `0${num}`;
+  }
+  return `${num}`;
+}
+
+function setText(selector, text) {
+  let targetElement = document.querySelector(selector);
+  targetElement.innerText = text;
+}
+
+let watchTime = () => {
+  const present = new Date();
+  const dayList = ["일", "월", "화", "수", "목", "금,", "토"];
+
+  const hour = leftPad(present.getHours());
+  const minute = leftPad(present.getMinutes());
+  const seconds = leftPad(present.getSeconds());
+  console.log(`${hour}-${minute}-${seconds}`);
+
+  setText("#hours", hour);
+  setText("#minute", minute);
+  setText("#seconds", seconds);
+
+  const year = present.getFullYear();
+  const month = leftPad(present.getMonth() + 1);
+  const date = leftPad(present.getDate());
+  const day = present.getDay();
+
+  setText("#year", year);
+  setText("#month", month);
+  setText("#date", date);
+  setText("#day", `${dayList[day]}요일`);
+};
+
+watchTime();
+
+let clockInterval = setInterval(watchTime, 1000);
